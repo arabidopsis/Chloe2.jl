80700ff78ab2a7792c153c9ec640560e28a55ef9
Ian Castleden <ian.castleden@uwa.edu.au>
2026-07-11 12:56:00 +0800

    chloe app

--- src/Chloe2.jl
+++ src/Chloe2.jl
@@ -18,6 +18,10 @@ export main, chloe, chloeone, writeGFF, tempfilename, TempFile
 const chloe2models = joinpath(artifact"Chloe2_models", "Chloe2_models-2.0.0-alpha.2", "models")
 const CHLOE_NAMESPACE = UUID("21d368ee-1647-4eec-a0c0-c42b8d8f3419")
 
+function (@main)(ARGS)
+    main(ARGS)
+end
+
 include("tempfile.jl")
 include("circularity.jl")
 include("feature.jl")
